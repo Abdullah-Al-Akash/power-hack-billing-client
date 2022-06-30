5cb16703ce3f77e7426b9fec4440c4be35481bee
Abdullah Al Akash <aabdullahalakash@gmail.com>
2022-06-30 11:30:49 +0600

    Table Haedline Complete

--- src/Pages/HomePage/HomePage.js
+++ src/Pages/HomePage/HomePage.js
@@ -7,6 +7,32 @@ const HomePage = () => {
                 <div>
                         <Navbar></Navbar>
                         <AddNewBill></AddNewBill>
+                        <div className="container mx-auto px-24 pt-4">
+                                <div class="overflow-x-auto">
+                                        <table class="table w-full">
+                                                <thead>
+                                                        <tr>
+                                                                <th>Billing ID</th>
+                                                                <th>Full Name</th>
+                                                                <th>Email</th>
+                                                                <th>Phone</th>
+                                                                <th>Paid Amount</th>
+                                                                <th>Action</th>
+                                                        </tr>
+                                                </thead>
+                                                <tbody>
+                                                        <tr>
+                                                                <th>1</th>
+                                                                <td>Cy Ganderton</td>
+                                                                <td>Quality Control Specialist</td>
+                                                                <td>Blue</td>
+                                                                <td>200</td>
+                                                                <td>Blue</td>
+                                                        </tr>
+                                                </tbody>
+                                        </table>
+                                </div>
+                        </div>
                 </div>
         );
 };
